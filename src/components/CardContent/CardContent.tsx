c6b883e759b61bf97c24fe5c460ff2e3422ebeb5
Kevin Fitzroy <kevinfitzroy715@gmail.com>
2020-09-18 01:41:47 +0800

    initial commit

---
+++ src/components/CardContent/CardContent.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const CardContent: React.FC = ({ children }) => (
+  <StyledCardContent>{children}</StyledCardContent>
+)
+
+const StyledCardContent = styled.div`
+background:#FFF4D7;
+  display: flex;
+  flex: 1;
+  flex-direction: column;
+  padding: ${(props) => props.theme.spacing[3]}px;
+`
+
+export default CardContent
---
+++ src/views/Home/Home.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import chef from '../../assets/img/chef.png'
+import Button from '../../components/Button'
+import Container from '../../components/Container'
+import Page from '../../components/Page'
+import PageHeader from '../../components/PageHeader'
+import Spacer from '../../components/Spacer'
+import Balances from './components/Balances'
+
+const Home: React.FC = () => {
+  return (
+    <Page>
+      <PageHeader
+        icon={<img src={chef} height={120} />}
+        title="HashChampion is Coming"
+        subtitle="Stake MinerSwap LP tokens to claim your very own great MST!"
+      />
+
+      <Container>
+        <Balances />
+      </Container>
+      <Spacer size="lg" />
+      <StyledInfo>
+        🚩<b>Pro Tip</b>:MST-ETH MST-USDT  MINER-V2 LP token pool yields TWICE more token rewards per block.
+      </StyledInfo>
+      <Spacer size="lg" />
+      <div
+        style={{
+          margin: '0 auto',
+        }}
+      >
+        <Button text="🔪 See the Menu" to="/farms" variant="secondary" />
+      </div>
+    </Page>
+  )
+}
+
+const StyledInfo = styled.h3`
+  color: ${(props) => props.theme.color.grey[500]};
+  font-size: 16px;
+  font-weight: 400;
+  margin: 0;
+  padding: 0;
+  text-align: center;
+
+  > b {
+    color: ${(props) => props.theme.color.grey[600]};
+  }
+`
+
+export default Home
